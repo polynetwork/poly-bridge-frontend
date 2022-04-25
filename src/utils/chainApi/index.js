83249b26e237270a2c8927191703239afb191bac
LinK <122826933@qq.com>
2022-04-25 19:01:38 +0800

    ontology evm init

--- src/utils/chainApi/index.js
+++ src/utils/chainApi/index.js
@@ -29,6 +29,7 @@ const APIS = {
   [ChainId.Oasis]: () => import('./eth'),
   [ChainId.Oasis1]: () => import('./eth'),
   [ChainId.Boba]: () => import('./eth'),
+  [ChainId.OntEvm]: () => import('./eth'),
 };
 
 export async function getChainApi(chainId) {
